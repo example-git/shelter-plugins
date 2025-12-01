49ad418904e5f38fbc502d05443aa71bb2906b3d
Sam <sam@xo.vg>
2025-12-01 07:41:03 -0500

    update2

--- lune.config.js
+++ lune.config.js
@@ -6,10 +6,10 @@ import { defineConfig } from "@uwu/lune";
 
 export default defineConfig({
   // this is the directory that your plugins live in.
-  // repoSubDir: "plugins-live-in-here",
+  repoSubDir: "plugins",
 
   // this enables CSS Module support - see docs for info
-  // cssModules: true,
+  cssModules: true,
 
   // these add extra esbuild plugins into the pipeline.
   // prePlugins: [],
